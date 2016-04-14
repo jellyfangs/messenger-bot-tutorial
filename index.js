1fee1e9becf2ed04dcfe4efb3aa1da89071eacb7
jw84 <jerrywang@me.com>
2016-04-13 23:10:43 -0700

    configuring express to parse req body

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express')
+var bodyParser = require('bodyParser')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extendd: false})
+
+// parse application/json
+app.use(bodyParser.json())
+
+// index
 app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
 })
 
+// for facebook verification
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
@@ -14,11 +23,13 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
+// to post data
 app.post('/webhook/', function (req, res) {
 	console.log(req.body)
 	res.sendStatus(200)
 })
 
+// spin spin sugar
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
