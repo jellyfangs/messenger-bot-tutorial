5c8e9d5a48f5ea0b98ad79ff99bc0101ce9c2046
jw84 <jerrywang@me.com>
2016-04-14 00:03:46 -0700

    postback

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ app.post('/webhook/', function (req, res) {
 			}
 			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
+		if (event.postback) {
+			text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			continue
+		}
 	}
 	res.sendStatus(200)
 })
