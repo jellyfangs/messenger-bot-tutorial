ed0d2c3c91d7bd0fa0204e54a6b78780ab2c23bd
jw84 <jerrywang@me.com>
2016-04-13 17:04:11 -0700

    oops

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
-appget('/webhook/', function (req, res) {
+app.post('/webhook/', function (req, res) {
 	messaging_events = req.body.entry[0].messaging
 	for (i = 0; i < messaging_events.length; i++) {
 		event = req.body.entry[0].messaging[i]
