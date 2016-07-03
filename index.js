041a153bea1b865556e98b2e3cff720cf9a2e952
prince355 <arvindj@hotmail.com>
2016-07-02 20:15:37 -0500

    password changed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === 'password') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token AJ')
