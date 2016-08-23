93bf97b76cc9dcc5455d1e20aff9a75067665004
Worwae77 <w.jumrassri@gmail.com>
2016-08-23 17:34:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,39 +43,40 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(sender)
 				continue
 			}
-			
-			if (text) {
-      // We received a text message
+			else
+			{
+			  // We received a text message
+			  const sender = messaging.sender.id
+			  const sessionId = findOrCreateSession(sender)
+			  // Let's forward the message to the Wit.ai Bot Engine
+			  // This will run all actions until our bot has nothing left to do
+			  wit.runActions(
+				sessionId, // the user's current session
+				msg, // the user's message 
+				sessions[sessionId].context, // the user's current session state
+				(error, context) => {
+				  if (error) {
+					console.log('Oops! Got an error from Wit:', error);
+				  } else {
+					// Our bot did everything it has to do.
+					// Now it's waiting for further messages to proceed.
+					console.log('Waiting for futher messages.');
 
-      // Let's forward the message to the Wit.ai Bot Engine
-      // This will run all actions until our bot has nothing left to do
-      wit.runActions(
-        sessionId, // the user's current session
-        msg, // the user's message 
-        sessions[sessionId].context, // the user's current session state
-        (error, context) => {
-          if (error) {
-            console.log('Oops! Got an error from Wit:', error);
-          } else {
-            // Our bot did everything it has to do.
-            // Now it's waiting for further messages to proceed.
-            console.log('Waiting for futher messages.');
+					// Based on the session state, you might want to reset the session.
+					// This depends heavily on the business logic of your bot.
+					// Example:
+					// if (context['done']) {
+					//   delete sessions[sessionId];
+					// }
 
-            // Based on the session state, you might want to reset the session.
-            // This depends heavily on the business logic of your bot.
-            // Example:
-            // if (context['done']) {
-            //   delete sessions[sessionId];
-            // }
-
-            // Updating the user's current session state
-            sessions[sessionId].context = context;
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
-			
-          }
-        }
-      );
-    }
+					// Updating the user's current session state
+					sessions[sessionId].context = context;
+					sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+					
+				  }
+				}
+			  );
+			}
 			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
