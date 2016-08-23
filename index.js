9ef4705f4a22349da7074df9f4e36c78155cd965
Worwae77 <w.jumrassri@gmail.com>
2016-08-23 17:44:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,27 @@ function sendTextMessage(sender, text) {
 		}
 	})
 }
-
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {
+      fbid: fbid,
+      context: {
+        _fbid_: fbid
+      }
+    }; // set context, _fid_
+  }
+  return sessionId;
+}
 
 // spin spin sugar
 app.listen(app.get('port'), function() {
