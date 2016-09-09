d0cce95e8707548f930ae63bbb3da19c3a9a3edc
Whelpley <whelpley@gmail.com>
2016-09-09 17:06:30 -0400

    Correct password

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ app.use(bodyParser.json())
 // index
 app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
+	// res.send('2028322212')
+
 })
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === 'hook_token') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
