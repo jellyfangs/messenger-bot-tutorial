35432dfde10f82628133498ca8b9ddc1256c5405
William Alan Henson <william.a.henson@gmail.com>
2017-05-03 11:20:16 -0600

    moved access token to config var

--- index.js
+++ index.js
@@ -63,8 +63,7 @@ app.post('/webhook/', function (req, res) {
 
 
 // recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "<FB_PAGE_ACCESS_TOKEN>"
+const token = process.env.FB_PAGE_ACCESS_TOKEN
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
